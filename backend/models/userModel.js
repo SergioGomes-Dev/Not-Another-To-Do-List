2889d566b3c59d310f0361209b440e53186fb550
Michael Gomes <michaelgomesweb@gmail.com>
2022-04-26 16:49:21 -0400

    User Registration & Password Encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -32,6 +32,17 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+//Before save encrypt password
+userSchema.pre("save", async function (next) {
+  // Checks if password has changed before encrypting it
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
