93733631a30923a83ed40215da6164e08e51f988
Michael Gomes <michaelgomesweb@gmail.com>
2022-04-26 15:28:27 -0400

    Redux on HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import List from "../components/List";
 import { Row, Col } from "react-bootstrap";
-import axios from "axios";
+import { listsAllAction } from "../actions/listActions";
 
 const HomeScreen = () => {
-  const [lists, setLists] = useState([]);
+  const dispatch = useDispatch();
+
+  const listsAll = useSelector((state) => state.listsAll);
+  const { loading, error, lists } = listsAll;
 
   useEffect(() => {
-    const fetchLists = async () => {
-      const { data } = await axios.get("/api/lists");
-
-      setLists(data);
-    };
-    fetchLists();
-  }, []);
+    dispatch(listsAllAction());
+  }, [dispatch]);
 
   return (
     <>
       <h1>All Lists</h1>
-      <Row>
-        {lists.map((list) => (
-          <Col key={list._id} sm={12} md={6} lg={4} xl={4}>
-            <List list={list} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : error ? (
+        <h2>{error}</h2>
+      ) : (
+        <Row>
+          {lists.map((list) => (
+            <Col key={list._id} sm={12} md={6} lg={4} xl={4}>
+              <List list={list} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
