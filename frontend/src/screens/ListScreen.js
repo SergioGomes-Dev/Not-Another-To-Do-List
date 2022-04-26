ac182c796bd8f5c70f740f30115d3b1deee78011
Michael Gomes <michaelgomesweb@gmail.com>
2022-04-26 15:39:23 -0400

    Redux on ListScreen

--- frontend/src/screens/ListScreen.js
+++ frontend/src/screens/ListScreen.js
@@ -1,40 +1,46 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 import { ListGroup, ListGroupItem, Card } from "react-bootstrap";
-import axios from "axios";
+import Loader from "../components/Loader";
+import Message from "../components/Message";
+import { listSingleAction } from "../actions/listActions";
 
 const ListScreen = () => {
-  const [list, setList] = useState({});
-
   const { id } = useParams();
 
+  const dispatch = useDispatch();
+
+  const listSingle = useSelector((state) => state.listSingle);
+  const { loading, error, list } = listSingle;
+
   useEffect(() => {
-    const fetchList = async () => {
-      const { data } = await axios.get(`/api/lists/${id}`);
-
-      setList(data);
-    };
-
-    fetchList();
-  }, [id]);
+    dispatch(listSingleAction(id));
+  }, [dispatch, id]);
 
   return (
     <>
       <Link className="btn btn-dark my-3" to="/" aria-label="go back button">
         Go Back
       </Link>
-      <Card className="list">
-        <Card.Title as="div" variant="top">
-          <strong>{list.name}</strong>
-        </Card.Title>
-        <ListGroup>
-          <>
-            {list.content?.map((i) => (
-              <ListGroupItem key={i._id}>{i.item}</ListGroupItem>
-            ))}
-          </>
-        </ListGroup>
-      </Card>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Card className="list">
+          <Card.Title as="div" variant="top">
+            <strong>{list.name}</strong>
+          </Card.Title>
+          <ListGroup>
+            <>
+              {list.content?.map((i) => (
+                <ListGroupItem key={i._id}>{i.item}</ListGroupItem>
+              ))}
+            </>
+          </ListGroup>
+        </Card>
+      )}
     </>
   );
 };
